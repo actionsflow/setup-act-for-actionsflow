3e5fffd4970572fbef2e720bedb99d4bd3b1e2d6
Owen Young <theowenyoung@gmail.com>
2021-01-27 05:38:53 +0800

    fix: install prompt

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const fs = require("fs").promises;
+const fsPure = require("fs")
+const fs = fsPure.promises;
 const path = require("path");
 const os = require("os");
 const core = require('@actions/core');
@@ -8,10 +9,17 @@ const toolVersion = core.getInput('version');
 run({
   version: toolVersion || DEFAULT_VERSION
 }).then(()=>{
-  // set rc
-  const actRC = `-P ubuntu-latest=actionsflow/act-environment:v1
--P ubuntu-18.04=actionsflow/act-environment:v1`;
-  return fs.writeFile(path.resolve(os.homedir(), ".actrc"), actRC);
+  // check if already exist
+  const actRcPath = path.resolve(os.homedir(), '.actrc');
+  const isActRcExist = fsPure.existsSync(actRcPath);
+  if(!isActRcExist){
+      // set rc
+      const actRC = `-P ubuntu-latest=actionsflow/act-environment:v1
+      -P ubuntu-18.04=actionsflow/act-environment:v1
+      -P ubuntu-20.04=actionsflow/act-environment:v1`;
+      return fs.writeFile(actRcPath, actRC);
+  }
+
 }).catch(e=>{
   core.setFailed(`Action failed with error ${e.message}`);
 })
